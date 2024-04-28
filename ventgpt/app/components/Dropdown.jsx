9cdb153dc64d21d9456c138a3deea4683cee4310
Paul Madut <paul.madut@mths.ca>
2024-04-27 22:03:23 -0400

    Drop down menu is looking somother

--- ventgpt/app/components/Dropdown.jsx
+++ ventgpt/app/components/Dropdown.jsx
@@ -1,22 +1,39 @@
-
-'use client'
-import React from 'react'
-import { useState } from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCaretUp, faCaretDown } from '@fortawesome/free-solid-svg-icons';
+"use client";
+import React from "react";
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCaretUp, faCaretDown } from "@fortawesome/free-solid-svg-icons";
 
 function Dropdown() {
-    const [isActive, setIsActive] = useState(false);
-
+  const [isActive, setIsActive] = useState(false);
 
   return (
-    <div className="container flex flex-col items-center w-48 rounded-xl  border-4 border-transparent hover:bg-gray-800 active:bg-gray-800 duration-500">
-        <button onClick={() => setIsActive((isActive) => !isActive)} className=' p-2 text-white  text-2xl font-bold tracking-wider'> VentGpt
-        {isActive ? <FontAwesomeIcon className='text-white px-4' icon={faCaretUp} /> : <FontAwesomeIcon className='text-white px-4' icon={faCaretDown} />}
-        </button>
-
+    <div className="container flex flex-col items-center w-48 rounded-xl  border-4 border-transparent hover:bg-[#2f2f2f] active:bg-gray-800 duration-500">
+      <button
+        onClick={() => setIsActive((isActive) => !isActive)}
+        className=" p-2 text-white  text-2xl font-bold tracking-wider"
+      >
+        {" "}
+        VentGpt
+        {isActive ? (
+          <FontAwesomeIcon className="text-white px-4" icon={faCaretUp} />
+        ) : (
+          <FontAwesomeIcon className="text-white px-4" icon={faCaretDown} />
+        )}
+      {isActive && (
+        <div className="text-white bg-[#2f2f2f] fixed left-auto rounded-lg mt-8 w-96 border-[1px] border-gray-800 ">
+          <br />
+          <div>
+            <p className=" text-sm text-left pl-4 py-2 text-[#a8a8a8]"> Therapist Model</p>
+            <ul>
+              <li></li>
+            </ul>
+          </div>
+        </div>
+      )}
+      </button>
     </div>
-  )
+  );
 }
 
-export default Dropdown
+export default Dropdown;
